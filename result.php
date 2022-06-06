e8369145488fb7b83a40ed3c99553eda9e56676c
sauravets <saurav.dubey.ets@gmail.com>
2022-06-06 19:36:10 +0530

    use-floor-function

--- result.php
+++ result.php
@@ -3,5 +3,12 @@
 if(isset($_POST['result'])){
     $first_input = $_POST['input1'];
     $second_input = $_POST['input2'];
+    $diff = abs(strtotime($first_input) - strtotime($second_input));
+
+    $week = floor($diff / (7*60*60*24));
+    $days = floor(($diff - $week * 7*60*60*24) / (60*60*24));
+    $hours = floor(($diff - $week * 7*60*60*24 - $days*60*60*24)/ (60*60));
+
+    printf("%d week, %d days, %d hours\n", $week, $days, $hours);
 }
 
