fa52ec8e2937fd80bd415b35c8fbf848cf9cf8cd
sauravets <saurav.dubey.ets@gmail.com>
2022-06-10 20:20:31 +0530

    complete-time-difference

--- result.php
+++ result.php
@@ -1,55 +1,68 @@
 <?php
-class Calculation {
+class Calculation
+{
+   public function create_string($array, $duration)
+   {
+      $new_string = '';
+      if (!empty($array) && isset($array[0][0]) && isset($array[1][0]) && isset($array[2][0]) && isset($array[3][0])) {
+         $new_string .= $array[0][0] . $array[1][0] . $array[2][0] . $array[3][0] . ".<br>";
+      } else {
+         $new_string = '0' . $duration;
+      }
+      return $new_string;
+   }
+   public function time_difference()
+   {
+      if (isset($_POST['result'])) {
+         $first_input = $_POST['input1'];
+         $second_input = $_POST['input2'];
 
-   public function time_difference(){
-    if(isset($_POST['result'])){
-       $first_input = $_POST['input1'];
-       $second_input = $_POST['input2'];
+         preg_match_all('((\d+(\.\d+)?)*(w))', $first_input, $week);
+         preg_match_all('((\d+(\.\d+)?)*(d))', $first_input, $day);
+         preg_match_all('((\d+(\.\d+)?)*(h))', $first_input, $hour);
 
-       preg_match_all('((\d+(\.\d+)?)*(w))',$first_input,$week);     
-       preg_match_all('((\d+(\.\d+)?)*(d))',$first_input,$day);
-       preg_match_all('((\d+(\.\d+)?)*(h))',$first_input,$hour);       
-       
-       preg_match_all('((\d+(\.\d+)?)*(w))',$second_input,$week1);
-       preg_match_all('((\d+(\.\d+)?)*(d))',$second_input,$day1);
-       preg_match_all('((\d+(\.\d+)?)*(h))',$second_input,$hour1);
+         preg_match_all('((\d+(\.\d+)?)*(w))', $second_input, $week1);
+         preg_match_all('((\d+(\.\d+)?)*(d))', $second_input, $day1);
+         preg_match_all('((\d+(\.\d+)?)*(h))', $second_input, $hour1);
 
-      //Get the value of week for first-input
-      $weeks =  $week[0][0].$week[0][1].$week[0][2].".<br>";
-      $week[1][0].$week[1][1].$week[1][2].".<br>";
 
-      //Get the value of day for first-input
-      $days =  $day[0][0].$day[0][1].$day[0][2].".<br>";
-      $day[1][0].$day[1][1].$day[1][2].".<br>";
+         // From first input
+         $weeks = $this->create_string($week, 'w');
+         $days = $this->create_string($day, 'd');
+         $hours =  $this->create_string($hour, 'h');
 
-      //Get the value of hour for first-input
-      $hours =  $hour[0][0].$hour[0][1].$hour[0][2].".<br>";
-      $hour[1][0].$hour[1][1].$hour[1][2].".<br>";
+         // From Second input
+         $weeks1 = $this->create_string($week1, 'w');
+         $days1 = $this->create_string($day1, 'd');
+         $hours1 =  $this->create_string($hour1, 'h');
 
-      //Get the value of week for second-input
-      $weeks1 =  $week1[0][0].$week1[0][1].$week1[0][2].".<br>";
-      $week1[1][0].$week1[1][1].$week1[1][2].".<br>";
-
-      //Get the value of day for second-input
-      $days1 =  $day1[0][0].$day1[0][1].$day1[0][2].".<br>";
-      $day1[1][0].$day1[1][1].$day1[1][2].".<br>";
-
-      //Get the value of hour for second-input
-      $hours1 =  $hour1[0][0].$hour1[0][1].$hour1[0][2].".<br>";
-      $hour1[1][0].$hour1[1][1].$hour1[1][2].".<br>";
-
-      $add = ($weeks * 40) + ($days * 8) + ($hours);
-      echo "<b>";
-      echo "first input value is:" . $add . "<br>";
-      $add1 = ($weeks1 * 40) + ($days1 * 8) + ($hours1);
-      echo "second input value is:" . $add1 . "<br>";       
-      echo "total result is:" . $add - $add1 ." hour";
-      echo "</b>";    
-    }
-    }
+         $add_first_input = ((int)$weeks * 40) + ((int)$days * 8) + ((int)$hours); //add first input
+         $add_second_input = ((int)$weeks1 * 40) + ((int)$days1 * 8) + ((int)$hours1); //add second input
+         $result =  $add_first_input - $add_second_input;    //calculate difference      
+         //calculate the week-    
+         $week_result = ($result / 40);
+         $round_week = round($week_result, 2);
+         $substr_week = substr($round_week, 1);
+         //calculate the day-
+         $day_result = ($substr_week * 40) / 8;
+         $round_day = round($day_result, 2);
+         $substr_day = substr($round_day, 1);
+         //calculate the hour-
+         $hour_result = ((int)$substr_day * 8);
+          
+         $string = '<b> The Output Is: ';
+         if (!empty($week_result) && (int)$week_result) {
+            $string .= (int)$week_result . "w ";
+         }
+         if(!empty($day_result) && (int)$day_result){
+            $string .= (int)$day_result . "d ";
+         }
+         if(!empty($hour_result) && (int)$hour_result){
+            $string .= (int)$hour_result . "h";
+         }
+         echo $string. "</br>";
+      }
+   }
 }
 
 $obj = new Calculation();
-
-
-
